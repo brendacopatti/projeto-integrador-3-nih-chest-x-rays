b137e7c5de253790032c97854d2d1dd76d4391dc
brendacopatti <brendascopatti@hotmail.com>
2021-10-06 21:43:21 -0300

    Faixa etaria

--- python-code/constant.py
+++ python-code/constant.py
@@ -11,12 +11,12 @@ SQL_DROP_PATIENT='drop table if exists patient'
 
 #CREATE TABLES
 SQL_CREATE_PATIENT='create table if not exists patient(id integer not null, gender char(1), constraint pk_patient primary key (id));'
-SQL_CREATE_FOLLOW_UP='create table if not exists follow_up(id integer not null,	patient_id integer not null,patient_age smallint,finding_label varchar(70),constraint pk_follow_up primary key (id, patient_id),constraint fk_patient foreign key (patient_id) references patient(id));'
+SQL_CREATE_FOLLOW_UP='create table if not exists follow_up(id integer not null,	patient_id integer not null, patient_age smallint, patient_age_group VARCHAR(20), finding_label varchar(70),constraint pk_follow_up primary key (id, patient_id),constraint fk_patient foreign key (patient_id) references patient(id));'
 SQL_CREATE_X_RAY='create table if not exists x_ray(follow_up_id integer not null, patient_id integer not null, view_position char(2), image bytea, original_image_width decimal(7,2),	original_image_height decimal(7,2),	original_image_pixels decimal(10,8), constraint pk_x_ray primary key (follow_up_id, patient_id), constraint fk_follow_up foreign key (follow_up_id, patient_id) references follow_up(id, patient_id));'
 
 #INSERT DATA
 SQL_INSERT_PATIENT='insert into patient(id, gender) values (%s, %s);'
-SQL_INSERT_FOLLOW_UP='insert into follow_up(id, patient_id, patient_age, finding_label) values (%s, %s, %s, %s);'
+SQL_INSERT_FOLLOW_UP='insert into follow_up(id, patient_id, patient_age, patient_age_group, finding_label) values (%s, %s, %s, %s, %s);'
 SQL_INSERT_X_RAY='insert into x_ray(follow_up_id, patient_id, view_position, image, original_image_width, original_image_height, original_image_pixels) values (%s, %s, %s, bytea(%s), %s, %s, %s)'
 
 #PROJECT
@@ -38,3 +38,12 @@ ORIGINAL_IMAGE_WIDTH='original_image_width',
 ORIGINAL_IMAGE_HEIGHT='original_image_height',
 ORIGINAL_IMAGE_SPACING='original_image_pixel_spacing'
 IMAGE_DIRECTORY_FILE_NAME='image_directory_file_name'
+PATIENT_AGE_GROUP='patient_age_group'
+
+#AGE GROUPS
+BABIES='Babies'
+CHILDREN='Children'
+YOUNG_ADULTS='Young adults'
+MIDDLE_AGED_ADULTS='Middle-aged adults'
+OLD_ADULTS='Old adults'
+
